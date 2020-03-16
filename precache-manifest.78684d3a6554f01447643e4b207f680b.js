d49682f62b673b2e5a3b53f9e6fc3a5b498f44d6
Ian <iantenet@gmail.com>
2020-03-16 12:30:25 +0800

    Updates

---
+++ precache-manifest.78684d3a6554f01447643e4b207f680b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb71fde4dbdb3414211d7de576825977",
+    "url": "/react-attendance/index.html"
+  },
+  {
+    "revision": "6c281a20a829a33b8374",
+    "url": "/react-attendance/static/css/main.c4998979.chunk.css"
+  },
+  {
+    "revision": "238de437b2e4210f6a8a",
+    "url": "/react-attendance/static/js/2.26a5cc86.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/react-attendance/static/js/2.26a5cc86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c281a20a829a33b8374",
+    "url": "/react-attendance/static/js/main.15e15e86.chunk.js"
+  },
+  {
+    "revision": "b68ca263f0e83da4eac5",
+    "url": "/react-attendance/static/js/runtime-main.5c349178.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-attendance/static/media/logo.5d5d9eef.svg"
+  }
+]);
